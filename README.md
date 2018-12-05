18a9246f8382f309c4b1654794091ca60147f3e4
Roya Amini-Naieni <ramininaieni@hmc.edu>
2018-12-05 13:20:40 -0800

    Add existing file

--- README.md
+++
@@ -1,5 +0,0 @@
-# GelElectrophoresisImages
-Make images with Gel Electrophoresis
-
-Virtual Environment is: gelElecEnv. 
-Run `source gelElecEnv/bin/activate` to activate the virtual environment.
---
+++ templateEqual.py
@@ -0,0 +1,41 @@
+#Steps for use:
+#(1)Export Benchling file as FASTA and put it in folder containing this code.
+#(2)Rename FASTA file as original file name with '.txt' ending.
+#(3)Find reference plasmid on Addgene and Export it into the same folder as FASTA file.
+#(4)Assign 'plasmidName' in templateEqual.py to the name of the plasmid used on Benchling. 
+#(5)Replace file names accordingly as 'addgenePlasmid' and 'benchlingPlasmid' in templateEqual.py.
+#(6)Run code!
+plasmidName = "pPSU1"
+with open('pPSU1Plasmid.fa', 'r') as addgenePlasmid:
+    data1 = addgenePlasmid.read().splitlines(True)
+addgeneOutput = ''
+for lineNumber in range(1, len(data1)):
+    data1[lineNumber].replace('\n', '')
+    for base in data1[lineNumber]:
+        addgeneOutput += base
+with open('ppsu1.fasta', 'r') as benchlingPlasmid:
+    data2=benchlingPlasmid.read().replace('>' + plasmidName, '')
+addgeneOutput = addgeneOutput.upper()
+consensusSeq = ''
+impurities = 0
+addgeneOutput = "".join(addgeneOutput.split()) 
+data2 = "".join(data2.split())
+if len(addgeneOutput) == len(data2):
+    for baseindex in range(0, len(data2)):
+        if addgeneOutput[baseindex] == data2[baseindex]:
+            consensusSeq+=addgeneOutput[baseindex]
+        else:
+            consensusSeq += '***'+addgeneOutput[baseindex].lower()+'***'
+            impurities+=1
+    if impurities > 0:
+        print("Benchling sequence differs from reference sequence at lower case base pairs surrounded by asterisks to make it easier to identify in the reference sequence below:")
+        print(consensusSeq)
+    else:
+        print("Benchling sequence matches reference sequence.")
+else:
+    print("Sequences are of different lengths listed below and thus unequal. We cannot perform the operation.")
+    print("benchling:")
+    print(len(data2))
+    print("addgene:")
+    print(len(addgeneOutput))
+    
